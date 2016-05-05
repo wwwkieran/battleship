486b797e25adbf57942b51917f3c06b6d8135757
Joseph <jhaggerty97@gmail.com>
2016-05-05 14:13:05 -0400

    board

---
+++ src/battleship/Board.java
@@ -0,0 +1,24 @@
+package battleship;
+
+public class Board {
+	
+	// instance variables
+	
+	int[][] board;
+	int NUM_COLUMNS = 10;
+	int NUM_ROWS = 10;
+	
+	// constructor
+	public Board() {
+		board = new int [NUM_ROWS][NUM_COLUMNS];
+	}
+	
+	//instance methods 
+	
+	public void clear() {
+		
+	}
+	
+	
+	
+}
