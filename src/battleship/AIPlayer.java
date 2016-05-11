4339dbf143f9fcaea1b80b3811e67846f9eb5b18
josephhaggerty <jhaggerty97@gmail.com>
2016-05-11 14:33:51 -0400

    added preliminary AI

--- src/battleship/AIPlayer.java
+++ src/battleship/AIPlayer.java
@@ -1,19 +1,53 @@
 package battleship;
 
+import java.util.*;
+
 public class AIPlayer {
 	
+	int[] returnArray;
+	boolean [][] alreadyVisited;
+	boolean hasFirstLocation;
+	protected int currentX;
+	protected int currentY;
+	Random rand;
+	
 	public AIPlayer() {
-		
+		returnArray = new int[2];
+		alreadyVisited = new boolean [10][10]; 
+		hasFirstLocation = false;
+		currentX = 0;
+		currentY = 0;
+		rand = new Random();
 	}
-
-	public int[] makeMove() {
-		/* Returns an array with the coordinates of a move to make in the format intArray[0] = x and intArray[1] = y */
-		int[] returnArray = new int[2];
-		int x = 0;
-		int y = 0;
+	/* Returns an array with the coordinates of a move to make in the format 
+	 * intArray[0] = x and intArray[1] = y 
+	 */
+	public int[] aiMakeMove() {
+		if(hasFirstLocation == false) {
+			int pickX = rand.nextInt(10) + 1;
+			int pickY = rand.nextInt(10) + 1;
+			while(alreadyVisited[pickX][pickY] == true) { 
+				pickX = rand.nextInt(10) + 1;
+				pickY = rand.nextInt(10) + 1;
+			}
+			currentX = pickX;
+			currentY = pickY;
+			alreadyVisited[pickX][pickY] = true;
+			hasFirstLocation = true;
+				
+			returnArray[0] = pickX;
+			returnArray[1] = pickY;
+			
 		
-		returnArray[0] = x;
-		returnArray[1] = y;
+		} else {
+			
+			
+			
+			
+			
+			
+		}
 		return returnArray;
 	}
 }
+		
